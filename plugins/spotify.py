ad509c2dfb35a0c87055d8fec7e6fd08f6d61f69
ihatevim <ihatevim@gmail.com>
2016-05-26 20:42:08 -0400

    Fixed rest of broken api stuffs

--- plugins/spotify.py
+++ plugins/spotify.py
@@ -41,64 +41,40 @@ def spotify(inp):
     except IndexError:
         return "Could not find track."
     url = sptfy(track)
-    if url == "Unable to shorten URL: There is a restriction of 10 seconds between submissions.":
-        url = track
-    return u"\x02{}\x02 by \x02{}\x02 - \x02{}\x02".format(data["tracks"]["items"][0]["name"],
-                                                           data["tracks"]["items"][0]["artists"][0]["name"], url)
+    uri = data["tracks"]["items"][0]["uri"]
+    return u"\x02{}\x02 by \x02{}\x02 - \x02{}\x02, {}".format(data["tracks"]["items"][0]["name"],
+                                                           data["tracks"]["items"][0]["artists"][0]["name"], url, uri)
 
 
 @hook.command
 def spalbum(inp):
     """spalbum <album> -- Search Spotify for <album>"""
     try:
-        data = http.get_json("http://ws.spotify.com/search/1/album.json", q=inp.strip())
+        data = http.get_json("http://api.spotify.com/v1/search?q=foo&type=album", q=inp.strip())
     except Exception as e:
         return "Could not get album information: {}".format(e)
 
     try:
-        type, id = data["albums"][0]["href"].split(":")[1:]
+        albumurl = data["albums"]["items"][0]["external_urls"]["spotify"]
     except IndexError:
         return "Could not find album."
-    url = sptfy(gateway.format(type, id))
-    return u"\x02{}\x02 by \x02{}\x02 - \x02{}\x02".format(data["albums"][0]["name"],
-                                                           data["albums"][0]["artists"][0]["name"], url)
+    url = sptfy(albumurl)
+    uri = data["albums"]["items"][0]["uri"]
+    return u"\x02{}\x02 - \x02{}\x02 - \x02{}\x02".format(data["albums"]["items"][0]["name"], url, uri)
 
 
 @hook.command
 def spartist(inp):
     """spartist <artist> -- Search Spotify for <artist>"""
     try:
-        data = http.get_json("http://ws.spotify.com/search/1/artist.json", q=inp.strip())
+        data = http.get_json("http://api.spotify.com/v1/search?q=foo&type=artist", q=inp.strip())
     except Exception as e:
         return "Could not get artist information: {}".format(e)
 
     try:
-        type, id = data["artists"][0]["href"].split(":")[1:]
+        artisturl = data["artists"]["items"][0]["external_urls"]["spotify"]
     except IndexError:
         return "Could not find artist."
-    url = sptfy(gateway.format(type, id))
-    return u"\x02{}\x02 - \x02{}\x02".format(data["artists"][0]["name"], url)
-
-
-@hook.regex(*http_re)
-@hook.regex(*spotify_re)
-def spotify_url(match):
-    type = match.group(2)
-    spotify_id = match.group(3)
-    url = spuri.format(type, spotify_id)
-    # no error catching here, if the API is down fail silently
-    data = http.get_json("http://ws.spotify.com/lookup/1/.json", uri=url)
-    if type == "track":
-        name = data["track"]["name"]
-        artist = data["track"]["artists"][0]["name"]
-        album = data["track"]["album"]["name"]
-        return u"Spotify Track: \x02{}\x02 by \x02{}\x02 from the album \x02{}\x02 - \x02{}\x02".format(name, artist,
-                                                                                                        album, sptfy(
-                gateway.format(type, spotify_id)))
-    elif type == "artist":
-        return u"Spotify Artist: \x02{}\x02 - \x02{}\x02".format(data["artist"]["name"],
-                                                                 sptfy(gateway.format(type, spotify_id)))
-    elif type == "album":
-        return u"Spotify Album: \x02{}\x02 - \x02{}\x02 - \x02{}\x02".format(data["album"]["artist"],
-                                                                             data["album"]["name"],
-                                                                             sptfy(gateway.format(type, spotify_id)))
+    url = sptfy(artisturl)
+    uri = data["artists"]["items"][0]["uri"]
+    return u"\x02{}\x02 - \x02{}\x02 - \x02{}\x02".format(data["artists"]["items"][0]["name"], url, uri)
